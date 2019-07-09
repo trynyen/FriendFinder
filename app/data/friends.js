c9915158a2ad48197ce1df5b98c85ec324cf55d5
trynyen <tryn.yen@gmail.com>
2019-07-08 19:58:08 -0500

    initial push with basic html, friends data, htmlroute created

---
+++ app/data/friends.js
@@ -0,0 +1,40 @@
+var friends = [
+    {
+        name: "Rachel Green",
+        photo: "../images/rachel.png",
+        scores: [2,1,3,2,1,2,2,3,5,5]
+    },
+    // https://66.media.tumblr.com/ff10cccf84e0b8bb41e50d2f2f96606b/tumblr_p3neo7jrtw1w8vpoto7_250.png
+    {
+        name:"Monica Geller",
+        photo:"./images/monica.png",
+        scores:[4,5,1,5,2,2,5,5,2,5]
+    },
+    // http://shegotofftheplane.weebly.com/uploads/5/8/8/1/58817427/9847352.png
+    {
+        name:"Phoebe Buffay",
+        photo:"./images/phoebe.png",
+        scores:[1,1,5,1,5,4,5,1,5,5]
+    },
+    //https://66.media.tumblr.com/40999f5c0ed99b505463216404bb091c/tumblr_pawi4kEwyr1w9ywnwo7_250.png
+    {
+        name:"Joey Tribbiani",
+        photo:"./images/joey.jpg",
+        scores:[1,3,5,1,5,5,2,1,4,5]
+    },
+    //https://pbs.twimg.com/media/CEKYKUVUsAAs0de.jpg
+    {
+        name:"Chandler Bing",
+        photo:"./images/chandler.png",
+        scores:[3,4,4,5,4,3,3,3,2,5]
+    },
+    //https://qph.fs.quoracdn.net/main-qimg-24aa438c0f0fbab6cef97e50b28efcd0
+    {
+        name:"Ross Geller",
+        photo:"./images/ross.jpg",
+        scores:[5,4,1,4,3,3,2,5,1,5]
+    },
+    //https://a.wattpad.com/useravatar/RossXGeller.256.97043.jpg
+]
+
+module.exports = friends;
