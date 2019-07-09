d5876c8592e7fa8d32b22316cef6efdb805cf927
trynyen <tryn.yen@gmail.com>
2019-07-08 23:03:25 -0500

    added descriptions

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,34 +1,35 @@
+//API friend list
 var friends = [
     {
         name: "Rachel Green",
         photo: "https://66.media.tumblr.com/ff10cccf84e0b8bb41e50d2f2f96606b/tumblr_p3neo7jrtw1w8vpoto7_250.png",
-        scores: [2,1,3,2,1,2,2,3,5,5]
+        scores: [2, 1, 3, 2, 1, 2, 2, 3, 5, 5]
     },
     {
-        name:"Monica Geller",
-    photo:"http://shegotofftheplane.weebly.com/uploads/5/8/8/1/58817427/9847352.png",
-        scores:[4,5,1,5,2,2,5,5,2,5]
+        name: "Monica Geller",
+        photo: "http://shegotofftheplane.weebly.com/uploads/5/8/8/1/58817427/9847352.png",
+        scores: [4, 5, 1, 5, 2, 2, 5, 5, 2, 5]
     },
     {
-        name:"Phoebe Buffay",
-        photo:"https://66.media.tumblr.com/40999f5c0ed99b505463216404bb091c/tumblr_pawi4kEwyr1w9ywnwo7_250.png",
-        scores:[1,1,5,1,5,4,5,1,5,5]
+        name: "Phoebe Buffay",
+        photo: "https://66.media.tumblr.com/40999f5c0ed99b505463216404bb091c/tumblr_pawi4kEwyr1w9ywnwo7_250.png",
+        scores: [1, 1, 5, 1, 5, 4, 5, 1, 5, 5]
     },
-    
+
     {
-        name:"Joey Tribbiani",
-        photo:"shttps://pbs.twimg.com/media/CEKYKUVUsAAs0de.jpg",
-        scores:[1,3,5,1,5,5,2,1,4,5]
-    },
-    {
-        name:"Chandler Bing",
-        photo:"https://qph.fs.quoracdn.net/main-qimg-24aa438c0f0fbab6cef97e50b28efcd0",
-        scores:[3,4,4,5,4,3,3,3,2,5]
+        name: "Joey Tribbiani",
+        photo: "shttps://pbs.twimg.com/media/CEKYKUVUsAAs0de.jpg",
+        scores: [1, 3, 5, 1, 5, 5, 2, 1, 4, 5]
     },
     {
-        name:"Ross Geller",
-        photo:"https://a.wattpad.com/useravatar/RossXGeller.256.97043.jpg",
-        scores:[5,4,1,4,3,3,2,5,1,5]
+        name: "Chandler Bing",
+        photo: "https://qph.fs.quoracdn.net/main-qimg-24aa438c0f0fbab6cef97e50b28efcd0",
+        scores: [3, 4, 4, 5, 4, 3, 3, 3, 2, 5]
+    },
+    {
+        name: "Ross Geller",
+        photo: "https://a.wattpad.com/useravatar/RossXGeller.256.97043.jpg",
+        scores: [5, 4, 1, 4, 3, 3, 2, 5, 1, 5]
     },
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,41 +1,60 @@
-
+//Require data from friends.js
 var friends = require("../data/friends.js");
 
-module.exports = function(app) {
-  
-  app.get("/api/friends", function(req, res) {
-    res.json(friends);
-  });
+module.exports = function (app) {
 
-  app.post("/api/friends", function(req, res) {
+    //API GET request when user visit a link, JSON data of friends are shown
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
 
-    var userData = req.body;
-    var userScores = userData.scores;
-    var bestMatch = 0;
-    var minimumDifference = 40;
+    //API POST request, when user submits survey
+    app.post("/api/friends", function (req, res) {
 
-    //Convert user score to number
-    for (var i = 0; i < userScores.length; i++){
-        userScores[i] = parseInt(userScores[i]);
-    };
+        //Set variables
+        //User Data is the request body (array)
+        var userData = req.body;
+        //User scores
+        var userScores = userData.scores;
+        //Set best match to first friend in the friend array (default)
+        var bestMatch = 0;
+        //Set minimumDifference to 40 
+        //because that's the maximum possible difference score between user scores and friend scores
+        var minimumDifference = 40;
 
-    //Loop through friends array
-    for (var i = 0; i < friends.length; i++){
-        var totalDifference = 0;
-        var friendScores = friends[i].scores
-        //Then loop through friend scores array
-        for (var j = 0; j < friendScores.length; j++){
-            var scoreDifference = Math.abs(userScores[j] - friendScores[j]);
-            totalDifference += scoreDifference;
+        //Convert user score to number
+        for (var i = 0; i < userScores.length; i++) {
+            userScores[i] = parseInt(userScores[i]);
+        };
+
+        //Loop through friends array
+        for (var i = 0; i < friends.length; i++) {
+            //At first set total score difference between user scores and friend scores to 0
+            var totalDifference = 0;
+            var friendScores = friends[i].scores
+            //Then loop through friend scores array
+            for (var j = 0; j < friendScores.length; j++) {
+                //Score difference equal userScores minus friendScores or vice versa
+                //By using Math.abs, we set an absolute value so the score would never be negative
+                var scoreDifference = Math.abs(userScores[j] - friendScores[j]);
+                //Add scoreDiffence to total Score Difference which was set to 0 at the beginning
+                totalDifference += scoreDifference;
+            }
+
+            //Everytime we got a total score difference, it will be compared to the minimum difference
+            //If it is smaller than the minimum difference, the a new minimum difference will be set to that total score difference
+            //Basically, we're comparing which friend has the closest total score to user score
+            //The minimum difference starts from 40 and will keep going down until we find the closet match
+            if (totalDifference < minimumDifference) {
+                bestMatch = i;
+                minimumDifference = totalDifference;
+            }
         }
-        if(totalDifference < minimumDifference){
-            bestMatch = i;
-            minimumDifference = totalDifference;
-        }
-    }
-    friends.push(userData);
+        //Push user Data to friend array after finding a match
+        friends.push(userData);
 
-    res.json(friends[bestMatch]);
-  });
+        //Send best match to browser
+        res.json(friends[bestMatch]);
+    });
 
 };
